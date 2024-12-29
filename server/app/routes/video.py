e354956942d16268f8331f082f1fde1edbec536e
Arghya <arghyadutta080@gmail.com>
2024-12-29 17:35:46 +0530

    fix: video route by adding req type

--- server/app/routes/video.py
+++ server/app/routes/video.py
@@ -3,6 +3,6 @@ from app.utils.video_stream import generate_frames
 
 video_bp = Blueprint('video', __name__)
 
-@video_bp.route('/feed')
+@video_bp.route('/feed', methods=['GET'])
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
